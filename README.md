8fd60f8be3ee69ce6e08312623c6a7892a95c120
Filipe Laíns <lains@riseup.net>
2023-03-17 02:38:29 +0000

    initial commit
    Signed-off-by: Filipe Laíns <lains@riseup.net>

---
+++ README.md
@@ -0,0 +1,5 @@
+# skeleton
+
+Inspired from:
+- https://github.com/jaraco/skeleton
+- https://github.com/dinoboff/skeleton
---
+++ docs/conf.py
@@ -0,0 +1,45 @@
+# SPDX-FileCopyrightText: 2023 Filipe Laíns
+#
+# SPDX-License-Identifier: MIT
+
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx_autodoc_typehints',
+    'sphinx_copybutton',
+    'sphinx_design',
+    'sphinxcontrib.spelling',
+    'sphinxext.opengraph',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'importlib_resources': ('https://importlib-resources.readthedocs.io/en/latest/', None),
+}
+
+templates_path = ['_templates']
+exclude_patterns = []
+default_role = 'any'
+autoclass_content = 'both'
+
+todo_include_todos = True
+
+html_theme = 'furo'
+html_title = f'{project} {version}'
+
+html_theme_options = {
+    'light_css_variables': {
+        'font-stack': (
+            'system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,'
+            'Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji'
+        ),
+    },
+}
+
+spelling_show_suggestions = True
+spelling_warning = True
+
+ogp_site_url = 'https://meson-python.readthedocs.io'
+ogp_site_name = 'meson-python documentation'
